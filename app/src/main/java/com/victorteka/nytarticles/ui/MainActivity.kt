79303bfbaf7582016d4e09e9d884e32b0d302f00
Victorteka <ambukateka@gmail.com>
2021-09-07 16:08:58 +0300

    add app module DI

--- app/src/main/java/com/victorteka/nytarticles/ui/MainActivity.kt
+++ app/src/main/java/com/victorteka/nytarticles/ui/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.victorteka.nytarticles.R
 import com.victorteka.nytarticles.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
