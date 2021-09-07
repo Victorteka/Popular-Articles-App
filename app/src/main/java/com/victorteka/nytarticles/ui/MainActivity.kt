bd661215a43db15733ec43d353d608075381f080
Victorteka <ambukateka@gmail.com>
2021-09-07 16:05:19 +0300

    add application class for hilt configuration

--- app/src/main/java/com/victorteka/nytarticles/ui/MainActivity.kt
+++ app/src/main/java/com/victorteka/nytarticles/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.victorteka.nytarticles
+package com.victorteka.nytarticles.ui
 
 import android.os.Bundle
 import android.view.Menu
@@ -11,6 +11,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import com.victorteka.nytarticles.R
 import com.victorteka.nytarticles.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
