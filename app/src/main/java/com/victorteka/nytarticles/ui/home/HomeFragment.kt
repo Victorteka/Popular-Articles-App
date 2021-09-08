bc0f183b48b3efb4034a36a97eb3f3da06d3e609
Victorteka <ambukateka@gmail.com>
2021-09-08 04:56:56 +0300

    Display articles on screen

--- app/src/main/java/com/victorteka/nytarticles/ui/home/HomeFragment.kt
+++ app/src/main/java/com/victorteka/nytarticles/ui/home/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.victorteka.nytarticles.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,9 +10,13 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.victorteka.domain.models.Article
 import com.victorteka.nytarticles.R
 import com.victorteka.nytarticles.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
+import com.victorteka.domain.Result
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
@@ -20,6 +25,7 @@ class HomeFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val homeViewModel by viewModels<HomeViewModel>()
+    private lateinit var adapter: PopularArticlesAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,15 +36,51 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        homeViewModel.articles.observe(viewLifecycleOwner, Observer {
-
-        })
+        setupUI()
+        setupData()
 
         return root
     }
 
-    private fun setupViewModel() {
+    private fun setupData() {
+        homeViewModel.articles.observe(viewLifecycleOwner){ result ->
+            when(result){
+                is Result.Error -> {
+                    binding.articlesProgressBar.visibility = View.GONE
+                    Log.d("HomeFragment", "${result.exception.message.orEmpty()}")
+                }
+                Result.Loading -> {
+                    Log.d("HomeFragment", "Loading ...")
+                    binding.articlesProgressBar.visibility = View.VISIBLE
+                }
+                is Result.Success -> {
+                    binding.articlesProgressBar.visibility = View.GONE
+                    renderList(result.data)
+                }
+            }
+        }
+    }
 
+    private fun setupUI() {
+        binding.popularArticlesRV.layoutManager = LinearLayoutManager(context)
+        adapter = PopularArticlesAdapter(arrayListOf())
+        binding.popularArticlesRV.addItemDecoration(
+            DividerItemDecoration(
+                binding.popularArticlesRV.context,
+                (binding.popularArticlesRV.layoutManager as LinearLayoutManager).orientation
+            )
+        )
+        binding.popularArticlesRV.adapter = adapter
+
+        /*adapter.on = { food ->
+            val action = AddNewMealFragmentDirections.actionAddNewMealFragmentToFoodDetailsFragment(food.food_name)
+            findNavController().navigate(action)
+        }*/
+    }
+
+    private fun renderList(articles: List<Article>){
+        adapter.addData(articles)
+        adapter.notifyDataSetChanged()
     }
 
     override fun onDestroyView() {
